895b572ffef839a1a09244c6f194a6fdd0f39a58
EmilyBowe <ebowe@tcd.ie>
2021-06-08 22:23:09 +0100

    players

--- src/main/kotlin/Player.kt
+++ src/main/kotlin/Player.kt
@@ -1,9 +1,17 @@
-class Player {
+class Player (val name: String)
     //array of players
     //$name of player
     //$currentSquare of player initialised to 0
     //$currentPlayer
 
+    val currentSquare = 0
 
 
+class Players (val players: Array<Player>) {
+    private var current = 0
+    fun currentPlayer() = players[current]
+    fun nextPlayer() {
+        current = if (current == players.lastIndex) 0 else ++current
+    }
 }
+
