708b573f8287cb653fa580f8a03d4eb0230a8023
EmilyBowe <ebowe@tcd.ie>
2021-06-09 00:21:45 +0100

    housekeeping

--- src/main/kotlin/Dice.kt
+++ src/main/kotlin/Dice.kt
@@ -1,6 +1,7 @@
 import java.security.SecureRandom
 
 //construct dice RNG 1-6
+//0 not valid - eliminate 0
 
 class Dice (val bound: Int) : Number {
     override fun generate() : Int{
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,28 +1,26 @@
-class Main {
+fun main(args: Array<String>) {
 
-    fun main(args: Array<String>) {
-
-        val board = Board(100)
-        val game = Game(board)
-        val snakesAndLadders = SnakesAndLadders(64, 54, 70, 80)
-        val players = Players(
-            arrayOf(
-                Player("A"),
-                Player("B")
-            )
+    val board = Board(100)
+    val game = Game(board)
+    val snakesAndLadders = SnakesAndLadders(64, 54, 70, 80)
+    val players = Players(
+        arrayOf(
+            Player("A"),
+            Player("B")
         )
-        players.apply {
-            while (!game.hasWinner(currentPlayer())) {
-                nextPlayer()
-                val turn = Turn(Dice(6))
-                turn.move(currentPlayer())
-                snakesAndLadders.updateCurrentSquareWithSnakeLadderIfFound(currentPlayer())
+    )
+    players.apply {
+        while (!game.hasWinner(currentPlayer())) {
+            nextPlayer()
+            val turn = Turn(Dice(6))
+            turn.move(currentPlayer())
+            snakesAndLadders.updateCurrentSquareWithSnakeLadderIfFound(currentPlayer())
 
-                if (game.hasWinner(currentPlayer())) {
-                    println("${currentPlayer().name} wins!!!!")
-                }
+            if (game.hasWinner(currentPlayer())) {
+                println("${currentPlayer().name} wins!!!!")
             }
         }
     }
 }
+
 
