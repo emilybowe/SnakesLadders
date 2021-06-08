90c0f85b8f4c2f0be20040c3b34b4c65e26231cb
EmilyBowe <ebowe@tcd.ie>
2021-06-08 21:44:08 +0100

    Board

--- src/main/kotlin/Board.kt
+++ src/main/kotlin/Board.kt
@@ -1,5 +1,14 @@
-class Board {
+class Board(val boardSize: Int) {
     //number container
     //board size passed in
     //board size = $winningSquare
+
+    val board = mutableListOf<Int>()
+    fun populateBoard() {
+        val boardRange = 1..boardSize
+        for (i in boardRange) {
+            board.add(i)
+        }
+    }
+
 }
